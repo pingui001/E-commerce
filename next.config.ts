f39278c81937f364dfe867870dbe3b18379b5868
pingui001 <nicolasporras0910@gmail.com>
2025-11-28 22:36:26 -0500

    fix: next.config.js para cargar imágenes externas

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-/**** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com', 
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
